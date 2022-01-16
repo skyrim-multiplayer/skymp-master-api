7fcc6f97acc15a65b40b32687cf8dee4445a513c
Roman Nikonov <11034375+nic11@users.noreply.github.com>
2022-01-17 02:05:04 +0300

    something bad before good code

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-FROM node:12
+FROM node:16
+#RUN mkdir -p /usr/src/app && chown www-data:www-data /usr/src/app
 WORKDIR /usr/src/app
-COPY package*.json ./
+COPY --chown=www-data:www-data package*.json tsconfig.json ./
+COPY data/stats0911.csv ./data/
 RUN npm install
-COPY . .
+COPY --chown=www-data:www-data src ./src
 EXPOSE 3000
+USER www-data
 CMD [ "npm", "run", "start" ]
---
+++ deploy/run_master_api.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -e
+
+die() {
+  echo "$@" >&2
+  exit 1
+}
+
+if [ ! -e Dockerfile ]; then
+  die run this script from repo root
+fi
+
+# cheatsheet: https://stackoverflow.com/a/44606194
+# ? without : accepts empty string
+# Ensuring all required variables are set:
+echo "${JWT_SECRET:?}" > /dev/null
+echo "${DB_URL:?}" > /dev/null
+echo "${EMAIL_USER:=}" > /dev/null
+echo "${EMAIL_PASS:=}" > /dev/null
+echo "${STATS_CSV_PATH:?}" > /dev/null
+echo "${S3_AWS_ACCESS_KEY_ID:=}" > /dev/null
+echo "${S3_AWS_SECRET_ACCESS_KEY:=}" > /dev/null
+echo "${DISCORD_CLIENT_ID:?}" > /dev/null
+echo "${DISCORD_CLIENT_SECRET:?}" > /dev/null
+
+docker build . --tag=skymp-master-api #|| die "Can't build image"
+
+[ -e data/stats.csv ] || echo "Time,PlayersOnline,ServersOnline" > data/stats.csv
+chmod o+w data/stats.csv
+
+docker run -d --restart=always \
+  -e PORT=3000 \
+  -e JWT_SECRET="$JWT_SECRET" \
+  -e DB_URL="$DB_URL" \
+  -e EMAIL_USER="$EMAIL_USER" \
+  -e EMAIL_PASS="$EMAIL_PASS" \
+  -e STATS_CSV_PATH="$STATS_CSV_PATH" \
+  -e IS_GITHUB_ACTION="$IS_GITHUB_ACTION" \
+  -e S3_AWS_ACCESS_KEY_ID="$S3_AWS_ACCESS_KEY_ID" \
+  -e S3_AWS_SECRET_ACCESS_KEY="$S3_AWS_SECRET_ACCESS_KEY" \
+  -e DISCORD_CLIENT_ID="$DISCORD_CLIENT_ID" \
+  -e DISCORD_CLIENT_SECRET="$DISCORD_CLIENT_SECRET" \
+  -v "$PWD/data:/data" \
+  -p 127.0.0.1:3000:3000 \
+  -v "$PWD/data/postgres_run:/var/run/postgresql" \
+  --name=skymp-master-api skymp-master-api
