61a54a8b83f173c5a32feeb20cebc5536a545e3e
Roman Nikonov <11034375+nic11@users.noreply.github.com>
2022-01-17 06:07:40 +0300

    Fix: Use MASTER_STATUS_CSV_PATH for target file

--- deploy/run_master_api.sh
+++ deploy/run_master_api.sh
@@ -14,7 +14,6 @@ echo "${JWT_SECRET:?}" > /dev/null
 echo "${DB_PASSWORD:?}" > /dev/null
 echo "${EMAIL_USER:=}" > /dev/null
 echo "${EMAIL_PASS:=}" > /dev/null
-echo "${STATS_CSV_PATH:?}" > /dev/null
 echo "${S3_AWS_ACCESS_KEY_ID:=}" > /dev/null
 echo "${S3_AWS_SECRET_ACCESS_KEY:=}" > /dev/null
 echo "${DISCORD_CLIENT_ID:?}" > /dev/null
@@ -39,7 +38,7 @@ docker run -d --restart=always \
   -e DB_URL="$DB_URL" \
   -e EMAIL_USER="$EMAIL_USER" \
   -e EMAIL_PASS="$EMAIL_PASS" \
-  -e STATS_CSV_PATH="$STATS_CSV_PATH" \
+  -e STATS_CSV_PATH="$MASTER_STATS_CSV_PATH" \
   -e IS_GITHUB_ACTION="$IS_GITHUB_ACTION" \
   -e S3_AWS_ACCESS_KEY_ID="$S3_AWS_ACCESS_KEY_ID" \
   -e S3_AWS_SECRET_ACCESS_KEY="$S3_AWS_SECRET_ACCESS_KEY" \
